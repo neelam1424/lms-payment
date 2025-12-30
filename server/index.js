5c15a790158f01d50750e546d0a72cdbd86abaca
neelam1424 <neelammore1404@gmail.com>
2025-12-30 17:46:07 -0500

    protecting with sql xxs and more

--- server/index.js
+++ server/index.js
@@ -1,12 +1,37 @@
 import express from 'express'
 import morgan from 'morgan'
-
+import rateLimit from 'express-rate-limit'
 import dotenv from "dotenv"
+import helmet from 'helmet'
+import mongoSanitize from 'express-mongo-sanitize'
+import hpp from 'hpp'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+
+
 dotenv.config()
 
 
 const app= express()
 const PORT =process.env.PORT;
+
+
+//Global rate limiting
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	message: "Too many request from this IP, please try later"
+})
+
+//security middleware
+app.use(helmet)
+app.use(mongoSanitize());
+app.use(hpp());
+app.use('/api',limiter);
+
+
+
+
 //logging middleware
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
@@ -17,6 +42,7 @@ if(process.env.NODE_ENV === 'development'){
 app.use(express.json({limit:'10kb'}))
 //accept data from url
 app.use(express.urlencoded({extended:true, limit:"10kb"}))
+app.use(cookieParser())
 
 
 
@@ -29,6 +55,22 @@ app.use((err, req,res,next)=>{
         ...(process.env.NODE_ENV === "development" && {stack: err.stack})
     })
 })
+
+//CORS configuration
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE","PATCH","HEAD","OPTIONS"],
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "X-Requested-With",
+        "device-remember-token",
+        "Access-Control-Allow-Origin",
+        "Origin",
+        "Accept",      
+    ]
+}))
 
 
 //API routes
